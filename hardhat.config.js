2bfd243c64ee1aaef9f1faf8d8cbb4adcc438cbf
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-01-22 17:49:53 -0500

    Updated config files and json for frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config()
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: '0.8.0',
@@ -9,4 +10,9 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY],
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
